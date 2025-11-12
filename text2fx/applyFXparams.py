fixing normalization bug

text2fx/applyFXparams.py
@@ -143,10 +143,6 @@ def apply_fx_to_sig(
                 
     # depending on exact json dict output, this will change
     print(f"Params dict: {params_dict}")
-    # params_list = torch.tensor([value for effect_params in params_dict.values() for value in effect_params.values()])
-    # if in_sig.batch_size != 1:
-    #     params_list = params_list.transpose(0, 1)
-    # params = params_list.expand(in_sig.batch_size, -1).to(DEVICE) #shape = (n_batch, n_params)
 
 
     params_list = torch.tensor(
@@ -158,11 +154,13 @@ def apply_fx_to_sig(
     batch_size = in_sig.batch_size
     params = params_list.view(1, -1).expand(batch_size, -1).to(DEVICE)
 
-
-    out_sig = fx_channel(in_sig.clone().to(DEVICE), params).normalize(-24)
+    
+    out_sig = fx_channel(in_sig.clone().to(DEVICE), params)
     if export_path:
         tc.export_sig(out_sig.clone().detach().cpu(), export_path)
-    return tc.preprocess_audio(out_sig.detach().cpu(), force_mono=False)
+
+    # return tc.preprocess_audio(out_sig.detach().cpu())
+    return out_sig.to_mono().normalize(-24).detach().cpu()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Apply effects to an audio file or dir of audio files based on parameters in a JSON file and export the processed file.")

text2fx/sap_main.py
@@ -110,7 +110,7 @@ def text2fx(
     clap = get_model(model_name) #default to ms_clap, though laion_clap might be better....
     print(f"Criterion: {criterion}")
 
-    sig = preprocess_audio(sig_in, force_mono=False).to(device) #preprocessing initial sample (entire sample)
+    sig = preprocess_audio(sig_in).to(device) #preprocessing initial sample (entire sample)
     # sig = preprocess_audio(sig_in, 5).to(device) #for fast version, taking 3s excerpt
 
     # FX parameter initialization
@@ -313,9 +313,8 @@ def text2fx(
     # Play final signal with optimized effects parameters
     clean_sig = preprocess_audio(sig_in).to(device) #taking full input sample 
     out_sig = channel(clean_sig.clone(), torch.sigmoid(params)).clone().detach().cpu()
-    out_sig = preprocess_audio(out_sig)  
+    out_sig = preprocess_audio(out_sig)  # ensure post-process
     out_params = params.detach().cpu() #optimized output FXparams
     out_params_dict = channel.save_params_to_dict(out_params) #mapping back to FX ranges
-
 
     return out_sig, out_params, out_params_dict